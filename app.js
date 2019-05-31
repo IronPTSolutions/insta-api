245f9709b23f711dabdf3e4e85a2346a7df7a7ae
Carlos del Prado <carlos.delprado@11paths.com>
2019-06-01 00:35:34 +0200

    chore: basic scaffolding

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((error, res, next) => {
+  console.error(error);
+  
+  res.status(error.status || 500);
+  const data = {};
+  data.message = error.message
+  
+  res.json(data);
+})
+
+module.exports = app;
